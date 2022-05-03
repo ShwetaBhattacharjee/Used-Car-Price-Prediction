2c6d9f4b75bd5c85e2179151d98cecc785ca599a
abc <you@example.com>
2022-05-03 16:16:14 +0600

    first commit

---
+++ 170204111_LinearRegression.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import sklearn as skl
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+
+
+dataset = pd.read_csv('car-data.csv')
+
+print(dataset.head())
+X = dataset[['Present_Price','Kms_Driven','Seating_Capacity','Car_Engine_cc','Top_speed_kmh','Age']]
+Y = dataset[['Selling_Price']]
+
+x = X.values
+y = Y.values
+
+
+x_train,x_test,y_train,y_test = train_test_split(x, y, test_size=0.66, random_state=42)
+
+
+from sklearn import linear_model
+lr = linear_model.LinearRegression()
+
+lr.fit(x_train, y_train)
+
+predicted=lr.predict(x_test)
+
+
+print("Predict value " + str(predicted))
+print("Real value " + str(y_test))
+print('r2_score: ',skl.metrics.r2_score(y_test,predicted)*100)
+print('explained_variance_score: ',skl.metrics.explained_variance_score(y_test,predicted)*100)
+print('neg_mean_gamma_deviance: ',skl.metrics.mean_gamma_deviance(y_test,predicted)*100)
+print('neg_mean_absolute_percentage_error',skl.metrics.mean_absolute_percentage_error(y_test,predicted)*100)
+
+
+
